df03edc5d9d3d0d7c1ad58cde70e5db4db78a1cd
darrenoje <darrenoje97@gmail.com>
2021-03-30 02:28:25 +0800

    input sanitisation and password hashing

--- project/server.py
+++ project/server.py
@@ -1,10 +1,81 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, flash
+from flask_sqlalchemy import SQLAlchemy
+import html, re, bcrypt
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+salt = bcrypt.gensalt()
 
-@app.route("/")
+db = SQLAlchemy(app)
+
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True) 
+    name = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
+
+
+def sanitiseInput(input):
+    sanitised = html.escape(input)
+    return sanitised
+
+def allowedPassword(input):
+    m = re.compile(r'[a-zA-Z0-9]*$')
+    if(m.match(input)):
+        return True
+    else:
+        return False
+
+@app.route("/create-acc", methods=["POST", "GET"])
+def create_acc():
+    if request.method == "POST":
+        user = request.form["nm"]
+        legal_password = allowedPassword(request.form["pw"])
+        
+        if legal_password:
+            password = request.form["pw"]
+            hashed_password = bcrypt.hashpw(password.encode(), salt)
+            new_user = users(user, hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash("Your account is successfully created!", "info")
+            return redirect(url_for("home")) 
+        else:
+            flash("Please ensure that your username contains only alphabets, and your password contains only alphabets and numbers!", "info")
+            return render_template("create-acc.html")
+    else:
+        return render_template("create-acc.html")
+
+@app.route("/", methods=["POST", "GET"])
 def home():
-    return render_template("index.html")
+    if request.method == "POST":
+        user = request.form["nm"]
+        password = sanitiseInput(request.form["pw"])
+        registered_user = users.query.filter_by(name=user).first()
+        if registered_user:
+            if bcrypt.checkpw(password.encode(), registered_user.password):    
+                return redirect(url_for("user"))    
+            else:
+                flash("Username or password is wrong! Kindly check again", "info")
+                return redirect(url_for("home"))
+        else:
+            flash("Username or password is wrong! Kindly check again", "info")
+            return render_template("index.html")
+    else:
+        return render_template("index.html")
+
+@app.route("/user")
+def user():
+    return f"<h1>Welcome!</h1>"
+
 
 if __name__ == "__main__":
-    app.run()
+    db.drop_all()
+    db.create_all()
+    app.run(debug=False)
+    
